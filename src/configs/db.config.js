3ced239c54afffc948738e6e78160d33a75e2fe9
DiegoG0477 <diego.14.13.lopez@gmail.com>
2023-11-21 07:11:14 -0600

    update

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -5,7 +5,7 @@ const config = {
     host:"localhost",
     user:"root",
     password:process.env.DB_PASSWORD,
-    database:"edu_planet",
+    database:"eduplanet",
 }
 
 const createConnection = async () =>{
