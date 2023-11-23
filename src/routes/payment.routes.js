63d883c344f52b9091c0bf729c53493eadfeca3a
david <daviddelacruzmorales42@gmail.com>
2023-11-23 00:45:26 -0600

    update transactions

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const middlewares = require('../middlewares/auth.middleware');
 const paymentController = require('../controllers/buys.controller');
 
-router.post("/create-order", paymentController.createOrder);
+router.post("/create-order", paymentController.create);
 router.get("/capture-order", paymentController.captureOrder);
 router.get("/cancel-payment", paymentController.cancelPayment);
 
